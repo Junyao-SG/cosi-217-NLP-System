aba8d2a06fd77d1b59a7af490b6eb3c34538d2b2
Junyao-SG <junyao0702@gmail.com>
2024-02-16 13:08:33 -0500

    updates fastapi

--- assignment_1/fastapi/main.py
+++ assignment_1/fastapi/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-import spacy
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
+import spacy
+import json
 
 
 app = FastAPI()
@@ -11,33 +12,43 @@ class TextModel(BaseModel):
 
 @app.get("/")
 def fastapi_spacy(pretty: bool = False):
-    if pretty:
-        return {
-            "Message": "This is a pretty page built on FastAPI to access spacy NER and dependency parsing."
-            }
+    message = {"Message": "This is a pretty page built on FastAPI to access spacy NER and dependency parsing."}
     
-    return {
-        "Message": "This isn't a pretty page to access spaCy NER and dependency parsing"
-        }
+    if pretty:
+        return prettify(message)
+    
+    return message
+
 
 @app.post("/ner")
 def named_entities_recognition(text_model: TextModel, pretty: bool = False):
     doc = nlp(text_model.text)
     entities = [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
 
+    ans = {"Entities": entities}
+
     if pretty:
-        return {"Given_Text": text_model.text,
-                "Entities": entities}
+        return prettify(ans)
     
-    return {"Entities": entities}
+    return ans
+
 
 @app.post("/dep")
 def dependency_parsing(text_model: TextModel, pretty: bool = False):
     doc = nlp(text_model.text)
     dependencies = [{"text": token.text, "dep": token.dep_, "head": token.head.text} for token in doc]
 
+    ans = {"Dependencies": dependencies}
+
     if pretty:
-        return {"Given_Text": text_model.text,
-                "Dependencies": dependencies}
+        return prettify(ans)
     
-    return {"Dependencies": dependencies}
+    return ans
+
+
+def prettify(result: dict):
+    '''
+    borrowed from given code
+    '''
+    json_str = json.dumps(result, indent=2)
+    return Response(content=json_str, media_type='application/json')
