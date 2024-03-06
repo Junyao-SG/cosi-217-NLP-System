3c769ae64fbcfbb6a18adb2fd68f970e9bb6a77e
Junyao-SG <junyao0702@gmail.com>
2024-03-05 23:18:49 -0500

    updates: add database with flask_sqlalchemy

--- assignment_2/app_flask.py
+++ assignment_2/app_flask.py
@@ -5,11 +5,24 @@ To see the pages point your browser at http://127.0.0.1:5000.
 """
 
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 import ner
 import utils
 
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ner.db'  # Update as needed
+db = SQLAlchemy(app)
+
+# Database Model
+class NERResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sentence = db.Column(db.String, nullable=False)
+    entity = db.Column(db.String, nullable=False)
+    entity_type = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
 
 
 @app.get('/')
@@ -28,9 +41,23 @@ def result():
             markup_paragraphed += '<p/>\n'
         else:
             markup_paragraphed += line
+    
+    for ent in doc.get_entities():
+        ner_result = NERResult(sentence=text, entity=ent[-1], entity_type=ent[-2])
+        db.session.add(ner_result)
+
+    db.session.commit()
+
     return render_template('result.html', markup=markup_paragraphed, dependencies=deps)
+
+@app.get('/data')
+def database():
+    all_data = NERResult.query.all()
+    return render_template('data.html', all_data=all_data)
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
 
     app.run(debug=True)
